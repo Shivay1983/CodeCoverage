a498b9f75016c9b7484e7da9f8c4366f5af92ce6
akyogi1983 <demouser@gmail.com>
2026-04-23 14:23:36 +0000

    Added gitleaks step

---
+++ agent01.py
@@ -0,0 +1,7 @@
+from strands import Agent
+
+# Create an agent using the Ollama model
+agent = Agent()
+
+# Use the agent
+agent("Tell me about Strands agents.") # Prints model output to stdout by default
