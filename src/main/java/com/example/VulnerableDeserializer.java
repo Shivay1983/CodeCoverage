03c2e827b2ab9df820f972d752a850a6e4858e6b
Shivay1983 <Shivay1983@users.noreply.github.com>
2026-04-25 18:54:58 +0530

    Update VulnerableDeserializer.java

--- src/main/java/com/example/VulnerableDeserializer.java
+++ src/main/java/com/example/VulnerableDeserializer.java
@@ -1,3 +1,14 @@
-// Vulnerable: using ObjectInputStream on untrusted data
-ObjectInputStream in = new ObjectInputStream(request.getInputStream());
-Object obj = in.readObject(); // unsafe
+package com.example;
+
+import java.io.ObjectInputStream;
+import java.io.InputStream;
+
+public class VulnerableDeserializer {
+
+    public Object deserialize(InputStream input) throws Exception {
+        // Vulnerable: using ObjectInputStream on untrusted data
+        ObjectInputStream in = new ObjectInputStream(input);
+        Object obj = in.readObject(); // unsafe
+        return obj;
+    }
+}
